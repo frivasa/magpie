13d9d86d79a0b3ff487fa880e51795f51e9200d8
rivas.aguilera@gmail.com <frivas1@tennessee.edu>
2017-09-30 20:43:46 -0400

    added tutorial, readme and function docs

--- magpie.py
+++ magpie.py
@@ -5,9 +5,18 @@ import numpy as np
 from subprocess import PIPE, Popen
 _mesa_dir = os.environ.get('MESA_DIR')
 _mesasdk_root = os.environ.get('MESASDK_ROOT')
-_maxmultipars = 10000000
+_maxmultipars = 10000
 
 class parameterGroup(object):
+    """class that handles all parameters for a run 
+    for browsing and editing inlists.
+    
+    Attributes:
+        params (dict): used parameters in the run separated 
+            by type ('controls', 'star_job', 'pgstar')
+        defaults (dict): every parameter available in the 
+            defaults files. Also split into types.
+    """
     types = ['controls', 'star_job', 'pgstar']
     
     def __init__(self):
@@ -23,11 +32,17 @@ class parameterGroup(object):
         else:
             return {}
 
-    def readDefaults(self, MESA_DIR):
+    def readDefaults(self):
+        """Read in all default parameters from the 
+        star/defaults folder. star_job.defaults has the best 
+        behaved syntax so docstrings should be correct.
+        Controls and pgstar however, are as random as they come 
+        so docstrings are seldom correct or available.
+        """
         if _mesa_dir is None:
             print("MESA_DIR not set, cannot read defaults.")
             return
-        globstr = MESA_DIR+"/star/defaults/"
+        globstr = _mesa_dir+"/star/defaults/"
         fnames = sorted([x for x in os.listdir(globstr) if ".defaults" in x])
         fpaths = [globstr+x for x in fnames]
         assert(len(fpaths)==3)
@@ -38,28 +53,53 @@ class parameterGroup(object):
             self.mixPars(t)
             
     def setPars(self, inlist, type='controls'):
-        """sets the parameters from an inlist file in the 
-        object. defaults to changing controls
+        """sets the parameters from an inlist in the 
+        object. Reads in only the 'type' parameters so if 
+        an inlist has multiple param types, run 'readInlist' 
+        instead.
+        
+        Args:
+            inlist (str): inlist to be read.
+            type (str): type of parameter to read in 
+                ('controls', 'star_job', 'pgstar').
+
         """
-        cd = dict(zip(*getRawPars(inlist, type=type)))
+        cd = dict(getRawPars(inlist, type=type))
         self.params[type].update(cd)
 
     def setDefs(self, parfile, type='controls'):
-        """reads in a .default file for check for altered values and 
-        docstrings on parameters
+        """reads in a .default file parameters and tries to 
+        get docstrings for each parameter.
+        
+        Args:
+            parfile (str): .default file to be read.
+            type (str): type of parameter to read in 
+                ('controls', 'star_job', 'pgstar').
+
         """
         n, v, d, err = getRawDefaults(parfile)
         cd = dict(zip(n, zip(v,d)))
         self.defaults[type].update(cd)
 
     def readInlist(self, inlist):
+        """reads in all parameters from an inlist
+        
+        Args:
+            inlist (str): inlist to be read.
+        
+        """
         self.setPars(inlist, type='controls')
         self.setPars(inlist, type='star_job')
         self.setPars(inlist, type='pgstar')
         
     def quickLook(self):
         """returns pandas dataframe with read inlist values for quick 
-        editing of parameters"""
+        editing of parameters.
+        
+        Returns:
+            pandas.core.frame.DataFrame: tabulated params (all types).
+        
+        """
         frames = []
         for t in parameterGroup.types:
             df = pd.DataFrame()
@@ -71,8 +111,15 @@ class parameterGroup(object):
         return pd.concat(frames)
         
     def tabulate(self, type):
-        """returns a pandas dataframe with parameters 
-        currently being used
+        """returns a pandas dataframe with parameters.
+        if defaults have been read, returns every parameter available 
+        with values from read inlist. This is a browsing mode.
+        if there's no defaults, show currently used parameters.
+        
+        Args:
+            type (str): type of parameters to show 
+                ('controls', 'star_job', 'pgstar').
+
         """
         df = pd.DataFrame()
         docs = ""
@@ -89,7 +136,9 @@ class parameterGroup(object):
         return df.set_index(type)
 
     def mixPars(self, type='controls'):
-        """sets the values of inlists into the default dictionary"""
+        """sets the values from inlists into the default 
+        dictionary.
+        """
         if not self.defaults[type]:
             return
         else:
@@ -101,6 +150,9 @@ class parameterGroup(object):
                     continue
 
     def readChanges(self, df):
+        """Takes an edited pandas dataframe and reads 
+        changed values.
+        """
         for t in parameterGroup.types:
             try:
                 cont = df[[t,'value']].dropna(0).set_index(t)
@@ -121,8 +173,15 @@ class parameterGroup(object):
 
     def writeInlists(self, outpath, sjname="star_job",
                      ctname="controls", pgname="pgstar"):
-        """Receives 3 dictionaries and builds a general inlist, plus 3
-        separate files called by it.
+        """Writes inlist files at 'outpath' using the object's params
+        values.
+        
+        Args:
+            outpath (str): path to work folder.
+            sjname (str): star_job inlist suffix.
+            ctname (str): controls inlist suffix.
+            pgname (str): pgstar inlist suffix.
+
         """
         for i, t in enumerate(parameterGroup.types):
             keys = ["read_extra_{}_inlist1", "extra_{}_inlist1_name"]
@@ -133,11 +192,12 @@ class parameterGroup(object):
                              clobber=True)
             else:
                 write2Inlist(inlistd, "&{}".format(t), outpath, "inlist")
-            write2Inlist(self[t], "&{}".format(t), outpath, xname, 
+            write2Inlist(self[t], "&{}".format(t), outpath, vals[1], 
                          clobber=True)
 
 
 def parseDocs(dlist):
+    """converts multiline docstrings into a single string."""
     nd = []
     for d in dlist:
         if isinstance(d, list):
@@ -147,8 +207,16 @@ def parseDocs(dlist):
     return nd
 
 
-def setupMESArun(codesource, destination, clobber=True):
-    """copy a target directory into a new one for editing the run"""
+def setupMESArun(destination, clobber=True):
+    """copy the star/work folder from your MESA distro  to 
+    a new folder to make a new run.
+    
+    Args:
+        destination (str): folder path for new run.
+        clobber (bool): overwrite the folder.
+    
+    """
+    codesource = os.path.join(_mesa_dir, "star/work")
     if os.path.exists(destination):
         if clobber:
             shutil.rmtree(destination)
@@ -157,16 +225,17 @@ def setupMESArun(codesource, destination, clobber=True):
         shutil.copytree(codesource, destination)
 
 
-def getRawPars(inlist, type="controls"):
-    """ gets a list of parameters from an inlist, subject to
-    type.
+def getRawPars(inlist, type='controls'):
+    """ gets a list of parameters from an inlist, 
+    subject to type.
 
     Args:
         inlist (str): input inlist.
         type (str): header type for parameters.
 
     Returns:
-        names (list), values (list)
+        list: [(name, value), ...]
+
     """
     pars = []
     add = 0
@@ -187,24 +256,24 @@ def getRawPars(inlist, type="controls"):
                     if '=' in line and '!' not in line:
                         l = line.strip().split('=')
                         pars.append((l[0].strip(),l[-1].strip()))
-    if len(pars)<1:
-        return [None], [None]
+    if not pars:
+        return []
     else:
-        return zip(*pars)
+        return pars
 
 
 def getRawDefaults(defaultfile):
     """ returns list of names, defaults and descriptions found for each 
     parameter in a .defaults file. if syntax is incorrect the parameter 
-    won't have a description but the realname will be returned for use.
-    Fills 'err' with the lines around the bad syntax 
-    parameters.
+    won't have a description but the name will still be returned for use.
+    Fills 'err' with the lines around the wrong syntax parameters.
 
     Args:
-        defaultfile (str): .default file
+        defaultfile (str): .default file.
     
     Returns:
-       names(list), defaults(list), descriptions(list), err(list)
+       list: names, list: values, list: docstrings, list: err.
+
     """
     pars, desc, multi = [], [], []
     name, val = "", ""
@@ -294,6 +363,15 @@ def getRawDefaults(defaultfile):
 
 
 def plainGenerator(length):
+    """simple integer counting generator.
+    
+    Args:
+        length (int): max count for generator.
+        
+    Yields:
+        int: counter from 0 to 'length'.
+    
+    """
     i = 0
     while i<length:
         yield i
@@ -303,6 +381,13 @@ def plainGenerator(length):
 def fortParse(arg):
     """returns a parsed variable from a parameter (bool,
     str, or number)
+    
+    Args:
+        arg (str): parameter value
+        
+    Returns:
+        str: decorated argument for fortran parsing.
+    
     """
     try:
         val = np.float(arg.replace('d','E'))
@@ -319,6 +404,14 @@ def fortParse(arg):
 def write2Inlist(parameters, header, outpath, fname, clobber=False):
     """Write paramater ditionary to file, appending for
     clobber=False (default)
+    
+    Args:
+        parameters (dict): dictionary of parameters.
+        header (str): type of parameters.
+        outpath (str): MESA work folder.
+        fname (str): inlist filename.
+        clobber (bool): rewrite file or append to file.
+    
     """
     if clobber:
         opt = 'w'
@@ -326,14 +419,18 @@ def write2Inlist(parameters, header, outpath, fname, clobber=False):
         opt = 'a'
     with open("/".join([outpath, fname]), opt) as o:
         o.write("\n{}\n\n".format(header))
-        for key in parameters.keys():
-            o.write("      {} = {}\n".format(key,
-                                       bu.fortParse(parameters[key])))
+        for k, v in parameters.items():
+            o.write("      {} = {}\n".format(k, fortParse(v)))
         o.write("\n/\n")
 
 
 def compileMESA(outpath, startsdk=True):
     """calls ./mk at outpath, compiling the run
+    
+    Args:
+        outpath (str): MESA work folder path.
+        startsdk (bool): source the mesasdk before compiling.
+    
     """
     if _mesa_dir is None:
         print("MESA_DIR not set. Cannot compile. Returning.")
